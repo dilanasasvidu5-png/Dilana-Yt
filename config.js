38ba82676281bb971c921737b4e8843951f9c6ad
dilanasasvidu5-png <dilanasasvidu5@gmail.com>
2025-08-30 19:29:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/dilanasasvidu5-png/Dilana-Yt/blob/main/images/Dark-land-rover-defender-2023.jpg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Dilana-Yt Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
